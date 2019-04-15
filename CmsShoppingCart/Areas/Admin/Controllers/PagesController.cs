dd7539ab0ca0f6c2faf3f4b12a500b6f5f71b2e3
tsacchet <tony_sacchetta@hotmail.com>
2019-04-16 09:43:11 +1000

    Admin pages fininshed

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,299 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // Declare list of Pages
+            List<PageVM> pagesList;
+
+            // Initialise the list
+            using (Db db = new Db()) {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            return View(pagesList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check Model state
+            if(! ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+
+            using (Db db = new Db())
+            {
+                // Declare slug
+                string slug;
+
+                // Init pageDTO
+                PageDTO dto = new PageDTO();
+
+                // DTO Title
+                dto.Title = model.Title;
+
+                // Check for and set slug if need be
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                // Make sure Title and Slug are unique
+                if(db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == model.Slug) )
+                {
+                    ModelState.AddModelError("", "That Title or Slug exist.");
+                    return View(model);
+                }
+
+                // DTO set the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                // Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+
+
+            }
+
+
+            // Set TempData message
+            TempData["SM"] = "You have add a new Page!";
+
+            // Redirect
+
+            return RedirectToAction("AddPage");
+        }
+
+
+        // GET: Admin/Pages/EditPages/id
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            // Declare pageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                // Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // Confirm page exits
+                if( dto == null)
+                {
+                    return Content("The page does not exit");
+                }
+                // Init pageVM
+                model = new PageVM(dto);
+            }
+
+
+            // Return view with model
+            return View(model);
+        }
+
+        // GET: Admin/Pages/EditPages/id
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            // Check Model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Get page id
+                int id = model.Id;
+
+                // Init slug
+                string slug="home";
+
+                // get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // DTO Title
+                dto.Title = model.Title;
+
+                // Check for and set slug if need be
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                // Make sure Title and Slug are unique
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) || 
+                    db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That Title or Slug already exist.");
+                    return View(model);
+                }
+
+                // DTO set the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                //dto.Sorting = 100;
+
+                // Save DTO
+                //db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            // Set TempData message
+            TempData["SM"] = "You have edited the Page!";
+
+            // Redirect
+            return RedirectToAction("EditPage");
+        }
+
+        // GET: Admin/Pages/PagesDetails/id
+        public ActionResult PageDetails(int id)
+        {
+            // Declare PageVM
+            PageVM model;
+
+            using(Db db = new Db())
+            {
+                // Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // Confirm page exists
+                if(dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                // Init PageVM
+                model = new PageVM(dto);
+
+            }
+
+            // retuen view with model
+            return View(model);
+        }
+
+        // GET: Admin/Pages/DeletePage/id
+        public ActionResult DeletePage(int id)
+        {
+
+            using (Db db = new Db())
+            {
+                // Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // Remove the page
+                db.Pages.Remove(dto);
+
+                // Save
+                db.SaveChanges();
+            }
+
+            // Redirect
+            return RedirectToAction("index");
+        }
+
+        // GET: Admin/Pages/ReorderPages
+        [HttpPost]
+        public void ReorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                // Set initiall count
+                int count = 1;
+
+                // Declare PageDTO
+                PageDTO dto;
+
+                // Set sorting for each page
+                foreach (var pageId in id)
+                {
+                    dto = db.Pages.Find(pageId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+        }
+
+        // GET: Admin/Pages/EditSidebar
+        [HttpGet]
+        public ActionResult EditSidebar()
+        {
+            // declare Model
+            SidebarVM model;
+
+            using (Db db = new Db())
+            {
+                // Get the DTO
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                // Init model
+                model = new SidebarVM(dto);
+            }
+
+            // return view with model
+            return View(model);
+        }
+
+        // POST: Admin/Pages/EditSidebar
+        [HttpPost]
+        public ActionResult EditSidebar(SidebarVM model)
+        {
+            using (Db db = new Db())
+            {
+                // Get the DTO
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                // DTO the body
+                dto.Body = model.Body;
+
+                // save
+                db.SaveChanges();
+            }
+
+            // Set TempData message
+            TempData["SM"] = "You have edited the sidebar!";
+
+            // Redirect
+            return RedirectToAction("EditSidebar");
+        }
+    }
+} 
---
+++ CmsShoppingCart/Models/ViewModels/Pages/SidebarVM.cs
@@ -0,0 +1,32 @@
+﻿using CmsShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Models.ViewModels.Pages
+{
+    public class SidebarVM
+    {
+        public SidebarVM()
+        {
+
+        }
+
+
+        public SidebarVM(SidebarDTO row)
+        {
+            Id = row.Id;
+            Body = row.Body;
+
+        }
+
+        public int Id { get; set; }
+
+        [Display(Name = "Sidebar")]
+        [AllowHtml]
+        public string Body { get; set; }
+    }
+}
